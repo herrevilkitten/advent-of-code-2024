de518809eefce0a2ec3b952dbc6800f74eab6d75
Eric Kidder <eric@kidderberg.us>
2024-12-03 17:10:38 -0500

    Cleaned it up a little

--- src/day3/main.ts
+++ src/day3/main.ts
@@ -18,14 +18,15 @@ function part2() {
   ].reduce<[number, boolean]>(
     (prev, curr) => {
       const [sum, enabled] = prev;
-      if (curr[0] === "do()") {
+      const [entire, ...numbers] = [curr[0], Number(curr[1]), Number(curr[2])];
+      if (entire === "do()") {
         return [sum, true];
-      } else if (curr[0] === "don't()") {
+      } else if (entire === "don't()") {
         return [sum, false];
       } else if (!enabled) {
         return prev;
       } else {
-        return [prev[0] + Number(curr[1]) * Number(curr[2]), enabled];
+        return [sum + numbers[0] * numbers[1], enabled];
       }
     },
     [0, true]
